b7d96ce02cf59a21028ee48c694a48d73e2e8e49
Ilya Lykov <ilya.lykov.program@gmail.com>
2025-01-31 16:25:55 +0400

    init: first commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { FlowersModule } from './flowers/flowers.module';
+
+@Module({
+  imports: [FlowersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
