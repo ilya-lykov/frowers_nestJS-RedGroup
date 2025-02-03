7f48392fe01b3f9a4493a6620979ad72a0bb3a0a
Ilya Lykov <ilya.lykov.program@gmail.com>
2025-02-03 13:50:13 +0400

    add: custom guard

--- src/flowers/flowers.controller.ts
+++ src/flowers/flowers.controller.ts
@@ -1,18 +1,15 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { FlowersService } from './flowers.service';
 import { ParseIntPipe } from 'src/conception/pipe';
+import { AuthGuard } from 'src/conception/guard';
 
 @Controller('flowers')
 export class FlowersController {
   constructor(private readonly flowersService: FlowersService) {}
 
-  // @Get('')
-  // findAll() {
-  //   return this.flowersService.findAll();
-  // }
+  @UseGuards(AuthGuard)
   @Get('')
-  findAll(@Query('pageNumber', ParseIntPipe) pageNumber: number) {
-    console.log(pageNumber);
+  findAll() {
     return this.flowersService.findAll();
   }
 }
